84b05cf90d494ea98cf842105bdf8f40e4667b31
sagzone <spkawad21@gmail.com>
2024-05-10 20:58:23 +0530

    onclick handler changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 export default function Navbar({ onNotesClickHandler, category }) {
   return (
-    <nav
-      onClick={onNotesClickHandler}
-      className="absolute bg-red-400 w-screen h-12 p-2"
-    >
+    <nav className="absolute bg-red-400 w-screen h-12 p-2">
       <div className="flex justify-center items-center">
-        <p className="absolute left-2">📓</p>
+        <p className="absolute left-2" onClick={onNotesClickHandler}>
+          📓
+        </p>
         <h1>{category}</h1>
       </div>
     </nav>
